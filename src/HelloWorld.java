ba9ce0e7fff115e49ad9e5b9b78ef54fb20ea5eb
Matthias <matthias.seitz@tum.de>
2017-11-06 13:56:54 +0100

    changed greeting

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("“Hello ASE2017 - how are you now? :)");
+		System.out.println("Please enter your name: ");
+		Scanner scanner = new Scanner(System.in);
+		HelloUser user = new HelloUser(scanner.nextLine().trim());
+		System.out.println(user.greetUser());
+		
 	}
 
 }
